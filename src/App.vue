5ef4024c0ac9d7df118a07fe5f1c851411744f26
Peter Olu <pemeraldy@gmail.com>
2022-03-19 07:42:24 +0100

    sidebar implemented

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 
 <template>
   <div class="flex justify-center bg-blue-800 p-5 w-screen min-h-screen">
-    <main class="bg-white w-full overflow-hidden min-h-full rounded-2xl py-2">
-      <nav class="flex justify-between border-b border-gray-300 p-2">
+    <main class="bg-white w-full overflow-hidden min-h-full rounded-2xl">
+      <nav class="flex justify-between border-b border-gray-300 p-4">
         <a href="#" class="inline-block w-20 h-6">
           <svg
             viewBox="0 0 233.21 72.63"
@@ -65,9 +65,89 @@
       </nav>
       <div class="flex bg-gray-50 h-full">
         <!-- sidebar -->
-        <div class="sm:w-32 border-r border-gray-300">
-          <div class="p-2 ">
-            <h1>Sideber</h1>
+        <div class="sm:w-[250px] border-r border-gray-300">
+          <div class="flex flex-col items-center">
+            <ul class="w-full pl-4">
+              <h2
+                class="px-2 py-2 border-b border-gray-100 uppercase text-gray-400 text-xs font-semibold w-full"
+              >
+                Tables
+              </h2>
+              <li v-for="i in 3" :key="i" class=" cursor-pointer pl-4  py-2 flex items-center">
+                <span class="inline-block w-4 h-4">
+                  <svg
+                    version="1.1"
+                    id="Layer_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 512 512"
+                    style="enable-background: new 0 0 512 512"
+                    xml:space="preserve"
+                  >
+                    <path
+                      style="fill: #ddd"
+                      d="M488.727,46.545H23.273C10.42,46.545,0,56.965,0,69.818v93.091V256v93.091v93.091
+	c0,12.853,10.42,23.273,23.273,23.273h155.152h155.152h155.152c12.853,0,23.273-10.42,23.273-23.273v-93.091V256v-93.091V69.818
+	C512,56.965,501.58,46.545,488.727,46.545z M155.152,418.909H46.545v-46.545h108.606V418.909z M155.152,325.818H46.545v-46.545
+	h108.606V325.818z M155.152,232.727H46.545v-46.545h108.606V232.727z M310.303,418.909H201.697v-46.545h108.606V418.909z
+	 M310.303,325.818H201.697v-46.545h108.606V325.818z M310.303,232.727H201.697v-46.545h108.606V232.727z M465.455,418.909H356.848
+	v-46.545h108.606V418.909z M465.455,325.818H356.848v-46.545h108.606V325.818z M465.455,232.727H356.848v-46.545h108.606V232.727z"
+                    />
+                    <path
+                      style="fill: #ddd"
+                      d="M488.727,46.545H23.273C10.42,46.545,0,56.965,0,69.818v93.091c0,12.853,10.42,23.273,23.273,23.273
+	h23.273h108.606h46.545h108.606h46.545h108.606h23.273c12.853,0,23.273-10.42,23.273-23.273V69.818
+	C512,56.965,501.58,46.545,488.727,46.545z"
+                    />
+                    <path
+                      style="fill: #ddd"
+                      d="M23.273,46.545C10.42,46.545,0,56.965,0,69.818v93.091c0,12.853,10.42,23.273,23.273,23.273h23.273
+	h108.606h46.545H256V46.545H23.273z"
+                    />
+                    <path
+                      style="fill: #ddd"
+                      d="M201.697,418.909v-46.545H256v-46.545h-54.303v-46.545H256v-46.545h-54.303v-46.545h-46.545v46.545
+	H46.545v-46.545H23.273C10.42,186.182,0,175.762,0,162.909V256v93.091v93.091c0,12.853,10.42,23.273,23.273,23.273h155.152H256
+	v-46.545H201.697z M155.152,418.909H46.545v-46.545h108.606V418.909z M155.152,325.818H46.545v-46.545h108.606V325.818z"
+                    />
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                  </svg>
+                </span>
+                <span class="ml-2 text-xs text-gray-700 font-medium">Customers</span>
+              </li>
+            </ul>
+            <!-- Saved queries -->
+            <ul class="w-full pl-4 mt-4">
+              <h2
+                class="px-2 py-2 border-b border-gray-100 uppercase text-gray-400 text-xs font-semibold w-full"
+              >
+                Saved Queries
+              </h2>
+              <li v-for="i in 3" :key="i" class="cursor-pointer pl-4 py-2 flex items-center">
+                <span class=" py-[2px] px-[3px] rounded text-[8px] font-semibold bg-orange-500 text-white"
+                  >SQL</span
+                >
+                <span class="ml-2 text-xs text-gray-700 font-medium"
+                  >All sold products</span
+                >
+              </li>
+            </ul>
           </div>
         </div>
         <!-- rest content -->
