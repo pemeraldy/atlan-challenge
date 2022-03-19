4ac796746eec4f86d08764b3a9e530ec39ba7e2b
Peter Olu <pemeraldy@gmail.com>
2022-03-19 20:42:06 +0100

    still on themeing editor

--- src/components/QueryHandler.vue
+++ src/components/QueryHandler.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="sm:h-[200px] w-full border-b border-gray-800 relative">
-    <!-- <textarea class="w-full h-full" id="editor" v-model="queryContent"></textarea> -->
-    <!-- <div id="editor"></div> -->
-    <VCodeMirror class=" w-full h-full" :value="queryContent" />
+  <div ref="codeEditor" class="sm:h-[200px] w-full border-b border-gray-800 relative">
+    <VCodeMirror class="w-full h-full" :mode="mode" :value="queryContent" />
+    <!-- :options="editorOptions" -->
+    <button class="text-white rounded absolute bg-blue-600 p-2 bottom-0">RunQuery</button>
   </div>
 </template>
 
 <script setup>
 import { VCodeMirror } from "vue3-code-mirror";
+import "../../node_modules/codemirror/theme/abbott.css";
 
 import { ref, onMounted } from "vue";
+const codeEditor = ref(null);
 const queryContent = ref("let a = 20;");
-onMounted(() => {});
+const mode = ref({ name: "javascript", json: true });
+const editorOptions = ref({
+  theme: "white",
+});
+onMounted(() => {
+  console.log(codeEditor.value);
+});
 </script>
