c089322cf2efba01c444253a73fc85a86064afa7
Peter Olu <pemeraldy@gmail.com>
2022-03-19 18:56:34 +0100

    removed some packeages

--- src/components/QueryHandler.vue
+++ src/components/QueryHandler.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="sm:h-[200px] w-full border-b border-gray-800">
-    <textarea class="w-full h-full" id="editor" v-model="queryContent"></textarea>
+  <div  class="sm:h-[200px] w-full border-b border-gray-800">
+    <!-- <textarea class="w-full h-full" id="editor" v-model="queryContent"></textarea> -->
+    <div id="editor"></div>
   </div>
 </template>
 
 <script setup>
-import * as CodeMirror from "codemirror";
-// import 'codemirror/lib/codemirror.css'
+
+
 import { ref, onMounted } from "vue";
 const queryContent = ref("let a = 20");
 onMounted(() => {
-  CodeMirror.fromTextArea(document.getElementById("editor"), {
-    lineNumbers: true,
-  });
-  console.log(CodeMirror);
+  
 });
 </script>
