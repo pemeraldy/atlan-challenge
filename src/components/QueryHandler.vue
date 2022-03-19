657c4328b399125fbb4edc0c5e907755f2acc723
Peter Olu <pemeraldy@gmail.com>
2022-03-19 19:56:07 +0100

    made codemirror respect parent container

--- src/components/QueryHandler.vue
+++ src/components/QueryHandler.vue
@@ -1,17 +1,15 @@
 <template>
-  <div  class="sm:h-[200px] w-full border-b border-gray-800">
+  <div class="sm:h-[200px] w-full border-b border-gray-800 relative">
     <!-- <textarea class="w-full h-full" id="editor" v-model="queryContent"></textarea> -->
     <!-- <div id="editor"></div> -->
-    <VCodeMirror value="let m = 3;" />
+    <VCodeMirror class=" w-full h-full" :value="queryContent" />
   </div>
 </template>
 
 <script setup>
-import {VCodeMirror}  from 'vue3-code-mirror'
+import { VCodeMirror } from "vue3-code-mirror";
 
 import { ref, onMounted } from "vue";
-const queryContent = ref("let a = 20");
-onMounted(() => {
-  
-});
+const queryContent = ref("let a = 20;");
+onMounted(() => {});
 </script>
