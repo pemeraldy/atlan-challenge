cfe7e057de92aa22e8594e22b174369161ef8e54
Peter Olu <pemeraldy@gmail.com>
2022-03-19 19:43:56 +0100

    vue3 code mirror integration

--- src/components/QueryHandler.vue
+++ src/components/QueryHandler.vue
@@ -1,12 +1,13 @@
 <template>
   <div  class="sm:h-[200px] w-full border-b border-gray-800">
     <!-- <textarea class="w-full h-full" id="editor" v-model="queryContent"></textarea> -->
-    <div id="editor"></div>
+    <!-- <div id="editor"></div> -->
+    <VCodeMirror value="let m = 3;" />
   </div>
 </template>
 
 <script setup>
-
+import {VCodeMirror}  from 'vue3-code-mirror'
 
 import { ref, onMounted } from "vue";
 const queryContent = ref("let a = 20");
