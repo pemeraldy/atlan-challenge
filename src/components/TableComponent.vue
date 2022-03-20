1db3ed12ef5436ea1e27ae5a2944638631963c0a
Peter Olu <pemeraldy@gmail.com>
2022-03-20 17:45:24 +0100

    uploaded JSON data files

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -2,7 +2,7 @@
 const props = defineProps({
   headers: {
     type: Array,
-    default: () => ["Product Name", "Category", "Price", "Edit"],
+    default: () => ["Product Name", "Category", "Price", "Action"],
   },
 });
 </script>
@@ -13,47 +13,42 @@ const props = defineProps({
       <div class="overflow-x-auto">
         <div class="inline-block min-w-full align-middle">
           <div class="overflow-hidden">
-            <table
-              class="min-w-full divide-y divide-gray-200 table-fixed"
-            >
-              <thead class="bg-gray-100 ">
+            <table class="min-w-full divide-y divide-gray-200 table-fixed">
+              <thead class="bg-gray-100">
                 <tr>
                   <th
                     v-for="(title, index) in props.headers"
                     :key="`${header}_${index}`"
                     scope="col"
-                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase "
+                    class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase"
                   >
                     {{ title }}
                   </th>
                 </tr>
               </thead>
-              <tbody
-                class="bg-white divide-y divide-gray-200 "
-              >
+              <tbody class="bg-white divide-y divide-gray-200">
                 <tr class="hover:bg-gray-100">
                   <td
-                    class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap "
+                    class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap"
                   >
                     Apple Imac 27"
                   </td>
                   <td
-                    class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap "
+                    class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap"
                   >
                     Desktop PC
                   </td>
                   <td
-                    class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap "
+                    class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap"
                   >
                     $1999
                   </td>
-                  <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                  <td class="py-4 px-6 text-sm font-medium whitespace-nowrap">
                     <a href="#" class="text-blue-600 dark:text-blue-500 hover:underline"
                       >Edit</a
                     >
                   </td>
                 </tr>
-                
               </tbody>
             </table>
           </div>
